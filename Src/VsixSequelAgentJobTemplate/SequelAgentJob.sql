0ee7e034db8aabccda32ab444197d613d9a51120
Greg Trevellick <gtrevellick@googlemail.com>
2020-04-20 21:45:11 +0100

    .

--- Src/VsixSequelAgentJobTemplate/SequelAgentJob.sql
+++ Src/VsixSequelAgentJobTemplate/SequelAgentJob.sql
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
-$endif$using System.Text;
+﻿begin tran
 
-namespace $rootnamespace$
-{
-	class $safeitemrootname$
-	{
-	}
-}
+print 'abc';
+
+rollback tran
