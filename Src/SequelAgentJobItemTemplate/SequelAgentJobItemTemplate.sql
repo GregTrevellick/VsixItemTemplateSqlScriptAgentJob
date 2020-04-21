ac80f58f3cab2dec500294dce31659fc117b58e0
Greg Trevellick <gtrevellick@googlemail.com>
2020-04-21 22:06:28 +0100

    .

--- Src/SequelAgentJobItemTemplate/SequelAgentJobItemTemplate.sql
+++ Src/SequelAgentJobItemTemplate/SequelAgentJobItemTemplate.sql
@@ -1,220 +1,178 @@
 ﻿BEGIN TRANSACTION
 
-DECLARE @ReturnCode INT
-SELECT @ReturnCode = 0
-DECLARE @UncategorizedLocal VARCHAR(30) = N '[Uncategorized (Local)]';
+	DECLARE @ReturnCode INT
+	SELECT @ReturnCode = 0
+	DECLARE @UncategorizedLocal VARCHAR(30) = N'[Uncategorized (Local)]';
 
---
--- Create category is not already exists
---
-IF NOT EXISTS (
-		SELECT [name]
-		FROM msdb.dbo.syscategories
-		WHERE [name] = @UncategorizedLocal
-			AND category_class = 1
-		)
-BEGIN
-	EXEC @ReturnCode = msdb.dbo.sp_add_category @class = N 'JOB'
-		,@type = N 'LOCAL'
-		,@name = @UncategorizedLocal
-	IF (@@ERROR <> 0 OR @ReturnCode <> 0)
-		GOTO QuitWithRollback
-END 
+	--
+	-- Create category is not already exists
+	--
+	IF NOT EXISTS (SELECT [name]
+					FROM msdb.dbo.syscategories
+					WHERE [name] = @UncategorizedLocal AND category_class = 1)
+	BEGIN
+		EXEC @ReturnCode = msdb.dbo.sp_add_category
+			 @class = N'JOB'
+			,@type = N'LOCAL'
+			,@name = @UncategorizedLocal
+		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+	END 
 
---
--- Initialise variables
---
-DECLARE @command VARCHAR(255);
-DECLARE @databaseName VARCHAR(255) = '#{Hades}OmitSquareBrackets';
-DECLARE @envChannel VARCHAR(255) = REPLACE(@databaseName, 'Hades', '');
-DECLARE @jobId BINARY (16);
-DECLARE @jobNamePrefix VARCHAR(255) = 'Primary Dev';
-DECLARE @jobName VARCHAR(255) = @jobNamePrefix + N ' Hades - Updater of things';
-DECLARE @scheduleName VARCHAR(255) = N 'Schedule-' + @jobName;
-DECLARE @stepId VARCHAR(255);
-DECLARE @stepName VARCHAR(255);
+	--
+	-- Initialise variables
+	--
+	DECLARE @command VARCHAR(255);                           --gregt nvarchar(max) ????
+	DECLARE @databaseName VARCHAR(255) = N'MyDb';
+	DECLARE @jobId BINARY (16);
+	DECLARE @jobName VARCHAR(255) = N'MyJobName' + '_' + @databaseName;
+	DECLARE @stepId VARCHAR(255);
+	DECLARE @stepName VARCHAR(255);
 
---            
---Create job (albeit empty) if not exists
---
-SELECT @jobId = job_id
-FROM msdb.dbo.sysjobs
-WHERE ([name] = @jobName)
+	--            
+	--Create job (albeit empty) if not exists
+	--
+	SELECT @jobId = job_id
+	FROM msdb.dbo.sysjobs
+	WHERE [name] = @jobName
 
-IF (@jobId IS NULL)
-BEGIN
-	EXEC @ReturnCode = msdb.dbo.sp_add_job @job_name = @jobName
-		,@enabled = 1
-		,
-		/* NOTE: 0=DISabled, 1=ENabled */
-		@notify_level_eventlog = 0
-		,@notify_level_email = 0
-		,@notify_level_netsend = 0
-		,@notify_level_page = 0
-		,@delete_level = 0
-		,@description = N 'Tux''s baby'
-		,@category_name = @UncategorizedLocal
-		,@owner_login_name = N 'sa'
-		,@job_id = @jobId OUTPUT
+	IF (@jobId IS NULL)
+	BEGIN
+		EXEC @ReturnCode = msdb.dbo.sp_add_job 
+			 @job_name = @jobName
+			,@enabled = 1 /* NOTE: 0=DISabled, 1=ENabled */
+			,@notify_level_eventlog = 0
+			,@notify_level_email = 0
+			,@notify_level_netsend = 0
+			,@notify_level_page = 0
+			,@delete_level = 0
+			,@description = N'My job description'
+			,@category_name = @UncategorizedLocal
+			,@owner_login_name = N'sa'
+			,@job_id = @jobId OUTPUT
+		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+	END
 
-	IF (@@ERROR <> 0 OR @ReturnCode <> 0)
-		GOTO QuitWithRollback
-END
+	--
+	-- Add job step(s) if does not exist
+	--            
 
---
--- Add job step(s) if does not exist
---            
+	SET @command = N'--My first sequel statement e.g. EXEC MyFirstSproc';
+	SET @stepId = 1;
+	SET @stepName = N'My first step';
+	IF (dbo.fnDoesJobStepExist(@jobName, @stepId) = 0)
+	BEGIN
+		EXEC @ReturnCode = msdb.dbo.sp_add_jobstep 
+			 @job_id = @jobId
+			,@step_id = @stepId
+			,@step_name = @stepName
+			,@cmdexec_success_code = 0
+			,@on_success_action = 3
+			,@on_success_step_id = 0
+			,@on_fail_action = 2
+			,@on_fail_step_id = 0
+			,@retry_attempts = 0
+			,@retry_interval = 0
+			,@os_run_priority = 0
+			,@subsystem = N'TSQL'
+			,@command = @command
+			,@database_name = @databaseName
+			,@flags = 0
+		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+	END
+	ELSE
+	BEGIN
+		EXEC msdb.dbo.sp_update_jobstep 
+			 @job_id = @jobId
+			,@step_id = @stepId	--The parameters below are an exact copy of those used in sp_add_jobstep above
+			,@step_name = @stepName
+			,@cmdexec_success_code = 0
+			,@on_success_action = 3
+			,@on_success_step_id = 0
+			,@on_fail_action = 2
+			,@on_fail_step_id = 0
+			,@retry_attempts = 0
+			,@retry_interval = 0
+			,@os_run_priority = 0
+			,@subsystem = N'TSQL'
+			,@command = @command
+			,@database_name = @databaseName
+			,@flags = 0
+		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+	END
 
-SET @command = N 'EXEC DimensionsSync_SyncAll';
-SET @stepId = 1;
-SET @stepName = N 'DimensionsSync_SyncAll';
+	SET @command = N'--My second sequel statement e.g. EXEC MySecondSproc';
+	SET @stepId = 2;
+	SET @stepName = N'My second step';
+	IF (dbo.fnDoesJobStepExist(@jobName, @stepId) = 0)
+	BEGIN
+		EXEC @ReturnCode = msdb.dbo.sp_add_jobstep 
+			 @job_id = @jobId
+			,@step_id = @stepId
+			,@step_name = @stepName
+			,@cmdexec_success_code = 0
+			,@on_success_action = 1
+			,@on_success_step_id = 0
+			,@on_fail_action = 2
+			,@on_fail_step_id = 0
+			,@retry_attempts = 0
+			,@retry_interval = 0
+			,@os_run_priority = 0
+			,@subsystem = N'TSQL'
+			,@command = @command
+			,@database_name = @databaseName
+			,@flags = 0
+		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+	END
+	ELSE
+	BEGIN
+		EXEC msdb.dbo.sp_update_jobstep 
+			 @job_id = @jobId
+			,@step_id = @stepId	--The parameters below are an exact copy of those used in sp_add_jobstep above
+			,@step_name = @stepName
+			,@cmdexec_success_code = 0
+			,@on_success_action = 1
+			,@on_success_step_id = 0
+			,@on_fail_action = 2
+			,@on_fail_step_id = 0
+			,@retry_attempts = 0
+			,@retry_interval = 0
+			,@os_run_priority = 0
+			,@subsystem = N'TSQL'
+			,@command = @command
+			,@database_name = @databaseName
+			,@flags = 0
+		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+	END 
 
-IF (dbo.fnDoesJobStepExist(@jobName, @stepId) = 0)
-BEGIN
-	EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id = @jobId
-		,@step_id = @stepId
-		,@step_name = @stepName
-		,@cmdexec_success_code = 0
-		,@on_success_action = 3
-		,@on_success_step_id = 0
-		,@on_fail_action = 2
-		,@on_fail_step_id = 0
-		,@retry_attempts = 0
-		,@retry_interval = 0
-		,@os_run_priority = 0
-		,@subsystem = N 'TSQL'
-		,@command = @command
-		,@database_name = @databaseName
-		,@flags = 0
-
-	IF (@@ERROR <> 0 OR @ReturnCode <> 0)
-		GOTO QuitWithRollback
-END
-ELSE
-BEGIN
-	EXEC msdb.dbo.sp_update_jobstep @job_id = @jobId
-		,@step_id = @stepId
-		,
-		--The parameters below are an exact copy of those used in sp_add_jobstep above
-		@step_name = @stepName
-		,@cmdexec_success_code = 0
-		,@on_success_action = 3
-		,@on_success_step_id = 0
-		,@on_fail_action = 2
-		,@on_fail_step_id = 0
-		,@retry_attempts = 0
-		,@retry_interval = 0
-		,@os_run_priority = 0
-		,@subsystem = N 'TSQL'
-		,@command = @command
-		,@database_name = @databaseName
-		,@flags = 0
-
-	IF (@@ERROR <> 0 OR @ReturnCode <> 0)
-		GOTO QuitWithRollback
-END
-
-SET @command = N 'EXEC PESSync_SyncAll';
-SET @stepId = 7;
-SET @stepName = N 'PESSync_SyncAll';
-
-IF (dbo.fnDoesJobStepExist(@jobName, @stepId) = 0)
-BEGIN
-	EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id = @jobId
-		,@step_id = @stepId
-		,@step_name = @stepName
-		,@cmdexec_success_code = 0
-		,@on_success_action = 1
-		,@on_success_step_id = 0
-		,@on_fail_action = 2
-		,@on_fail_step_id = 0
-		,@retry_attempts = 0
-		,@retry_interval = 0
-		,@os_run_priority = 0
-		,@subsystem = N 'TSQL'
-		,@command = @command
-		,@database_name = @databaseName
-		,@flags = 0
-
-	IF (@@ERROR <> 0 OR @ReturnCode <> 0)
-		GOTO QuitWithRollback
-END
-ELSE
-BEGIN
-	EXEC msdb.dbo.sp_update_jobstep @job_id = @jobId
-		,@step_id = @stepId
-		,
-		--The parameters below are an exact copy of those used in sp_add_jobstep above
-		@step_name = @stepName
-		,@cmdexec_success_code = 0
-		,@on_success_action = 1
-		,@on_success_step_id = 0
-		,@on_fail_action = 2
-		,@on_fail_step_id = 0
-		,@retry_attempts = 0
-		,@retry_interval = 0
-		,@os_run_priority = 0
-		,@subsystem = N 'TSQL'
-		,@command = @command
-		,@database_name = @databaseName
-		,@flags = 0
-
-	IF (@@ERROR <> 0 OR @ReturnCode <> 0)
-		GOTO QuitWithRollback
-END 
-
---
--- Set job start step id (not sure if truly needed but when scripting out an existing job this always appears)
---
-EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId
-	,@start_step_id = 1
-
-IF (@@ERROR <> 0 OR @ReturnCode <> 0)
-	GOTO QuitWithRollback 
+	--
+	-- Set job start step id (not sure if truly needed but when scripting out an existing job this always appears)
+	--
+	EXEC @ReturnCode = msdb.dbo.sp_update_job 
+ 		 @job_id = @jobId
+		,@start_step_id = 1
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback 
 	
-	--Schedules should be set up manually, hence commented out below.
-	--This avoids shared schedule id's across environments / channels.
-	--Also means we can create the job in an enabled state if we want, without the job automatically running potentially against our will.
-	--EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=@scheduleName,
-	--                             @enabled=1,  /* NOTE: 0=DISabled, 1=ENabled */
-	--                             @freq_type=4,
-	--                             @freq_interval=1,
-	--                             @freq_subday_type=4,
-	--                             @freq_subday_interval=5,
-	--                             @freq_relative_interval=0,
-	--                             @freq_recurrence_factor=0,
-	--                             @active_start_date=20191024, /* If copying this file for another agent job use a new date here */
-	--                             @active_end_date=99991231,
-	--                             @active_start_time=0,
-	--                             @active_end_time=235959,
-	--                             @schedule_uid=N'addd36db-7433-4010-ab08-d3825c280078' /* If copying this file for another agent job use a new guid here */
-	--IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 	--
 	-- sp_add_jobserver (not sure what this does, but when scripting out an existing job this always appears)
 	--
+	BEGIN TRY
+		EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId
+			,@server_name = N'(local)'
+		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+	END TRY
 
-BEGIN TRY
-	EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId
-		,@server_name = N '(local)'
-
-	IF (@@ERROR <> 0 OR @ReturnCode <> 0)
-		GOTO QuitWithRollback
-END TRY
-
-BEGIN CATCH -- Ignore error 14269 ("Job 'xxxxx' is already targeted at server 'MB-SQL-X-XX'")
-	IF ((SELECT ERROR_NUMBER() AS ErrorNumber) <> 14269)
-	BEGIN
-		GOTO QuitWithRollback
-	END
-END CATCH;
+	BEGIN CATCH -- Ignore error 14269 ("Job 'xxxxx' is already targeted at server 'MB-SQL-X-XX'")
+		IF ((SELECT ERROR_NUMBER() AS ErrorNumber) <> 14269)
+		BEGIN
+			GOTO QuitWithRollback
+		END
+	END CATCH;
 
 COMMIT TRANSACTION
-
 GOTO EndSave 
 
 QuitWithRollback:
-IF (@@TRANCOUNT > 0)
-	ROLLBACK TRANSACTION 
+IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION 
 	
 EndSave:
 GO
