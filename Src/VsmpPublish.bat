620de3c1a4f7d672d6507fe1fa07f12a4b5c7100
Greg Trevellick <gtrevellick@googlemail.com>
2020-04-24 17:54:34 +0100

    .

--- Src/VsmpPublish.bat
+++ Src/VsmpPublish.bat
@@ -1,5 +1,10 @@
+CD D:\_Dgit\_MINE_ACTIVE\OpenInApp.Launcher\src\packages\Microsoft.VSSDK.BuildTools.16.4.1057\tools\vssdk\bin 
+
+.\VsixPublisher.exe publish -payload "..\..\..\..\..\..\..\VsixItemTemplateSqlScriptAgentJob\Src\ItemTemplate.VsixPackage\bin\Debug\\VsixSequelAgentJob.vsix" -publishManifest "..\..\..\..\..\..\..\VsixItemTemplateSqlScriptAgentJob\Src\VsmpPublish.json" -personalAccessToken "vsmp_pat"
+
+REM "vsmp_pat"
+
+
+
 REM https://docs.microsoft.com/en-us/visualstudio/extensibility/walkthrough-publishing-a-visual-studio-extension-via-command-line?view=vs-2017
-
-CD D:
-
-"D:\_Dgit\_MINE_ACTIVE\OpenInApp.Launcher\src\packages\Microsoft.VSSDK.BuildTools.15.8.3252\tools\vssdk\bin\VsixPublisher.exe" publish -payload "D:\_Dgit\_MINE_ACTIVE\VsixItemTemplateSqlScriptAgentJob\Src\ItemTemplate.VsixPackage\bin\Debug\\VsixSequelAgentJob.vsix" -publishManifest "D:\_Dgit\_MINE_ACTIVE\VsixItemTemplateSqlScriptAgentJob\Src\VsmpPublish.json" -personalAccessToken "vsmp_pat"
+REM https://docs.microsoft.com/en-us/visualstudio/extensibility/walkthrough-publishing-a-visual-studio-extension-via-command-line?view=vs-2019
