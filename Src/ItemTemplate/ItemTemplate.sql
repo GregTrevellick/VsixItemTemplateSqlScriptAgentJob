1fca3f9b6850d4e94659b40d0b8a54f19c4af090
Greg Trevellick <gtrevellick@googlemail.com>
2020-05-02 18:34:59 +0100

    simpler

--- Src/ItemTemplate/ItemTemplate.sql
+++ Src/ItemTemplate/ItemTemplate.sql
@@ -1,60 +1,7 @@
-﻿IF EXISTS
-(
-    SELECT *
-    FROM sys.objects
-    WHERE object_id = OBJECT_ID(N'fnDoesJobStepExist')
-)
-BEGIN
-    DROP FUNCTION fnDoesJobStepExist;
-END;
-GO
- 
- 
-CREATE FUNCTION [dbo].fnDoesJobStepExist
-(
-                @jobName VARCHAR(255),
-                @stepId VARCHAR(255)
-)
-RETURNS BIT
-BEGIN
-   DECLARE @output BIT;
-                IF EXISTS
-                               (SELECT 1
-                              FROM [msdb].[dbo].[sysjobs] a WITH(NOLOCK)
-                               INNER JOIN [msdb].[dbo].[sysjobsteps] b WITH(NOLOCK) ON a.job_id = b.job_id
-                               WHERE a.[Name] = @jobName and b.step_id = @stepId)
-               BEGIN
-                              SET @output = 1;
-               END
-               ELSE
-               BEGIN
-                               SET @output = 0;
-                END
-    RETURN @output;
-END;
-
-
-
-
-BEGIN TRANSACTION
+﻿BEGIN TRANSACTION
 
 	DECLARE @ReturnCode INT
 	SELECT @ReturnCode = 0
-	DECLARE @UncategorizedLocal VARCHAR(30) = N'[Uncategorized (Local)]';
-
-	--
-	-- Create category is not already exists
-	--
-	IF NOT EXISTS (SELECT [name]
-					FROM msdb.dbo.syscategories
-					WHERE [name] = @UncategorizedLocal AND category_class = 1)
-	BEGIN
-		EXEC @ReturnCode = msdb.dbo.sp_add_category
-			 @class = N'JOB'
-			,@type = N'LOCAL'
-			,@name = @UncategorizedLocal
-		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-	END 
 
 	--
 	-- Initialise variables
@@ -62,7 +9,10 @@ BEGIN TRANSACTION
 	DECLARE @command VARCHAR(255);                           --gregt nvarchar(max) ????
 	DECLARE @databaseName VARCHAR(255) = N'MyDb';
 	DECLARE @jobId BINARY (16);
+
+
 	DECLARE @jobName VARCHAR(255) = N'MyJobName' + '_' + @databaseName;
+
 	DECLARE @stepId VARCHAR(255);
 	DECLARE @stepName VARCHAR(255);
 
@@ -78,13 +28,7 @@ BEGIN TRANSACTION
 		EXEC @ReturnCode = msdb.dbo.sp_add_job 
 			 @job_name = @jobName
 			,@enabled = 1 /* NOTE: 0=DISabled, 1=ENabled */
-			,@notify_level_eventlog = 0
-			,@notify_level_email = 0
-			,@notify_level_netsend = 0
-			,@notify_level_page = 0
-			,@delete_level = 0
 			,@description = N'My job description'
-			,@category_name = @UncategorizedLocal
 			,@owner_login_name = N'sa'
 			,@job_id = @jobId OUTPUT
 		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
@@ -97,21 +41,17 @@ BEGIN TRANSACTION
 	SET @command = N'--My first sequel statement e.g. EXEC MyFirstSproc';
 	SET @stepId = 1;
 	SET @stepName = N'My first step';
-	IF (dbo.fnDoesJobStepExist(@jobName, @stepId) = 0)
+	IF NOT EXISTS 
+		(SELECT 1
+		FROM [msdb].[dbo].[sysjobs] a WITH(NOLOCK)
+		INNER JOIN [msdb].[dbo].[sysjobsteps] b WITH(NOLOCK) ON a.job_id = b.job_id 
+		WHERE a.[Name] = @jobName and b.step_id = @stepId)
 	BEGIN
 		EXEC @ReturnCode = msdb.dbo.sp_add_jobstep 
 			 @job_id = @jobId
 			,@step_id = @stepId
 			,@step_name = @stepName
-			,@cmdexec_success_code = 0
 			,@on_success_action = 3
-			,@on_success_step_id = 0
-			,@on_fail_action = 2
-			,@on_fail_step_id = 0
-			,@retry_attempts = 0
-			,@retry_interval = 0
-			,@os_run_priority = 0
-			,@subsystem = N'TSQL'
 			,@command = @command
 			,@database_name = @databaseName
 			,@flags = 0
@@ -123,39 +63,29 @@ BEGIN TRANSACTION
 			 @job_id = @jobId
 			,@step_id = @stepId	--The parameters below are an exact copy of those used in sp_add_jobstep above
 			,@step_name = @stepName
-			,@cmdexec_success_code = 0
-			,@on_success_action = 3
-			,@on_success_step_id = 0
-			,@on_fail_action = 2
-			,@on_fail_step_id = 0
-			,@retry_attempts = 0
-			,@retry_interval = 0
-			,@os_run_priority = 0
-			,@subsystem = N'TSQL'
+			,@on_success_action = 3 --gregt dedupe
 			,@command = @command
 			,@database_name = @databaseName
 			,@flags = 0
 		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 	END
 
+	/* LAST STEP */
+	/* LAST STEP */
+	/* LAST STEP */
 	SET @command = N'--My second sequel statement e.g. EXEC MySecondSproc';
 	SET @stepId = 2;
 	SET @stepName = N'My second step';
-	IF (dbo.fnDoesJobStepExist(@jobName, @stepId) = 0)
+	IF NOT EXISTS 
+		(SELECT 1
+		FROM [msdb].[dbo].[sysjobs] a WITH(NOLOCK)
+		INNER JOIN [msdb].[dbo].[sysjobsteps] b WITH(NOLOCK) ON a.job_id = b.job_id 
+		WHERE a.[Name] = @jobName and b.step_id = @stepId)
 	BEGIN
 		EXEC @ReturnCode = msdb.dbo.sp_add_jobstep 
 			 @job_id = @jobId
 			,@step_id = @stepId
 			,@step_name = @stepName
-			,@cmdexec_success_code = 0
-			,@on_success_action = 1
-			,@on_success_step_id = 0
-			,@on_fail_action = 2
-			,@on_fail_step_id = 0
-			,@retry_attempts = 0
-			,@retry_interval = 0
-			,@os_run_priority = 0
-			,@subsystem = N'TSQL'
 			,@command = @command
 			,@database_name = @databaseName
 			,@flags = 0
@@ -167,15 +97,6 @@ BEGIN TRANSACTION
 			 @job_id = @jobId
 			,@step_id = @stepId	--The parameters below are an exact copy of those used in sp_add_jobstep above
 			,@step_name = @stepName
-			,@cmdexec_success_code = 0
-			,@on_success_action = 1
-			,@on_success_step_id = 0
-			,@on_fail_action = 2
-			,@on_fail_step_id = 0
-			,@retry_attempts = 0
-			,@retry_interval = 0
-			,@os_run_priority = 0
-			,@subsystem = N'TSQL'
 			,@command = @command
 			,@database_name = @databaseName
 			,@flags = 0
