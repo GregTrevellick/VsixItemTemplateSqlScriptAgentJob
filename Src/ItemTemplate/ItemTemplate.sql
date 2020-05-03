b69e1f359d2b85b7abfc632d5a8dcf5615692c21
Greg Trevellick <gtrevellick@googlemail.com>
2020-05-03 10:39:11 +0100

    .

--- Src/ItemTemplate/ItemTemplate.sql
+++ Src/ItemTemplate/ItemTemplate.sql
@@ -1,88 +1,98 @@
 ﻿BEGIN TRANSACTION
 
 	-- Variables
-	--gregt make variables start with a capital
-	DECLARE @command NVARCHAR(MAX);
-	DECLARE @databaseName SYSNAME = N'MyDb';
-	DECLARE @jobId UNIQUEIDENTIFIER;                                                ---------------------------BINARY (16);
-	DECLARE @jobName SYSNAME = N'MyJobName' + '_' + @databaseName;                      ------------VARCHAR(255) = 
-    DECLARE @on_success_action_GoToNextStep TINYINT = 3; -- https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-add-jobstep-transact-sql?view=sql-server-ver15#arguments 
+	DECLARE @Command NVARCHAR(MAX);
+	DECLARE @DatabaseName SYSNAME = N'MyDatabase';
+	DECLARE @JobDescription nvarchar(512) =  N'My job description';
+	DECLARE @JobId UNIQUEIDENTIFIER;
+	DECLARE @JobName SYSNAME = N'MyJobName' + '_' + @DatabaseName;
+    DECLARE @OnSuccessAactionGoToNextStep TINYINT = 3;
+	DECLARE @OwnerLoginName SYSNAME = N'sa';
 	DECLARE @ReturnCode INT = 0;
-	DECLARE @stepId INT;
-	DECLARE @stepName SYSNAME;
+	DECLARE @ServerName NVARCHAR(30) = N'(local)';
+	DECLARE @StepId INT;
+	DECLARE @StepName SYSNAME;
 
 	-- Create job (https://docs.microsoft.com/en-us/sql/ssms/agent/create-a-job)
-	SELECT @jobId = job_id FROM msdb.dbo.sysjobs WHERE [name] = @jobName
-	IF (@jobId IS NULL)
+	SELECT @JobId = job_id FROM msdb.dbo.sysjobs WHERE [name] = @JobName
+	IF (@JobId IS NULL)
 	BEGIN
+		-- Add job (https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-add-job-transact-sql)
 		EXEC @ReturnCode = msdb.dbo.sp_add_job 
-				 @description = N'My job description'
-				--,@enabled = 0 -- Uncomment to create the job in disabled state
-				,@job_id = @jobId OUTPUT
-				,@job_name = @jobName
-				,@owner_login_name = N'sa'
+				 @description = @JobDescription
+				--,@enabled = 0 -- Uncomment to create job in disabled state
+				,@job_id = @JobId OUTPUT
+				,@job_name = @JobName
+				,@owner_login_name = @OwnerLoginName
+		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 		-- Add target server (https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-add-jobserver-transact-sql)
-		EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+		EXEC @ReturnCode = msdb.dbo.sp_add_jobserver
+				 @job_id = @JobId 
+				,@server_name = @ServerName
 		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 	END
 
-	-- Add first job step (https://docs.microsoft.com/en-us/sql/ssms/agent/create-a-transact-sql-job-step)
-	SET @command = N'EXEC MySproc';
-	SET @stepId = 1;
-	SET @stepName = N'MySproc';
+	-- Create first job step https://docs.microsoft.com/en-us/sql/ssms/agent/create-a-transact-sql-job-step
+	SET @Command = N'EXEC MySproc';
+	SET @StepId = 1;
+	SET @StepName = N'MySproc';
 	IF NOT EXISTS 
 		(SELECT 1
 		FROM msdb.dbo.sysjobs j WITH(NOLOCK)
 		INNER JOIN msdb.dbo.sysjobsteps s WITH(NOLOCK) ON j.job_id = s.job_id 
-		WHERE j.[Name] = @jobName and s.step_id = @stepId)
+		WHERE j.[Name] = @JobName and s.step_id = @StepId)
 	BEGIN
+		-- Add step https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-add-jobstep-transact-sql
 		EXEC @ReturnCode = msdb.dbo.sp_add_jobstep 
-				 @command = @command
-				,@database_name = @databaseName
-				,@job_id = @jobId
-				,@on_success_action = @on_success_action_GoToNextStep
-				,@step_id = @stepId
-				,@step_name = @stepName
+				 @Command = @Command
+				,@database_name = @DatabaseName
+				,@job_id = @JobId
+				,@on_success_action = @onSuccessAactionGoToNextStep
+				,@step_id = @StepId
+				,@step_name = @StepName
 		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 	END
 	ELSE
 	BEGIN
+		-- Update step https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-update-jobstep-transact-sql
 		EXEC @ReturnCode = msdb.dbo.sp_update_jobstep 
-				 @command = @command
-				,@database_name = @databaseName
-				,@job_id = @jobId
-				,@on_success_action = @on_success_action_GoToNextStep
-				,@step_id = @stepId	
-				,@step_name = @stepName
+				 @Command = @Command
+				,@database_name = @DatabaseName
+				,@job_id = @JobId
+				,@on_success_action = @onSuccessAactionGoToNextStep
+				,@step_id = @StepId	
+				,@step_name = @StepName
 		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 	END
 
-	-- Add last job step
-	SET @command = N'EXEC MyLastSproc';
-	SET @stepId = 2;
-	SET @stepName = N'MyLastSproc';
+	-- Create last job step https://docs.microsoft.com/en-us/sql/ssms/agent/create-a-transact-sql-job-step	
+	SET @Command = N'EXEC MyLastSproc';
+	SET @StepId = 2;
+	SET @StepName = N'MyLastSproc';
 	IF NOT EXISTS 
 		(SELECT 1
 		FROM msdb.dbo.sysjobs j WITH(NOLOCK)
 		INNER JOIN msdb.dbo.sysjobsteps s WITH(NOLOCK) ON j.job_id = s.job_id 
-		WHERE j.[Name] = @jobName and s.step_id = @stepId)
+		WHERE j.[Name] = @JobName and s.step_id = @StepId)
 	BEGIN
+		-- Add step (https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-add-jobstep-transact-sql)
 		EXEC @ReturnCode = msdb.dbo.sp_add_jobstep 
-				 @command = @command
-				,@database_name = @databaseName
-				,@job_id = @jobId
-				,@step_id = @stepId
-				,@step_name = @stepName
+				 @Command = @Command
+				,@database_name = @DatabaseName
+				,@job_id = @JobId
+				,@step_id = @StepId
+				,@step_name = @StepName
 		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 	END
 	ELSE
 	BEGIN
+		-- Update step (https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-update-jobstep-transact-sql)
 		EXEC @ReturnCode = msdb.dbo.sp_update_jobstep 
-				 @command = @command
-				,@database_name = @databaseName
-				,@job_id = @jobId
-				,@step_id = @stepId	--The parameters below are an exact copy of those used in sp_add_jobstep above
-				,@step_name = @stepName
+				 @Command = @Command
+				,@database_name = @DatabaseName
+				,@job_id = @JobId
+				,@step_id = @StepId	
+				,@step_name = @StepName
 		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 	END 
 
